39a1e1e2d07e814737ece303ee0ce3706429d00a
Hugo <hquezada@leomail.tamuc.edu>
2025-09-03 13:34:30 -0500

    feat: install and set up cors.

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const indexRoutes = require("./routes/indexRoutes");
 const productsRoutes = require("./routes/productRoutes");
 const requestRoutes = require("./routes/requestRoutes");
 const contactRoutes = require("./routes/contactRoutes");
+const cors = require("cors");
 // const aboutRoutes = require("./routes/aboutRoutes");
 
 // Parse JSON for API requests
 app.use(express.json());
 
+// Use the cors middleware
+app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:5173", // or whatever your React app's port is
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
 // API routes
 app.use("/", indexRoutes);
 app.use("/products", productsRoutes);
 app.use("/request-a-quote", requestRoutes);
+app.use("/", requestRoutes);
 app.use("/contact", contactRoutes);
 // app.use("/about", aboutRoutes);
 
--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const requestRouter = Router();
 const requestController = require("../controllers/requestController");
 
-requestRouter.post("/", requestController);
+// requestRouter.post("/", requestController);
+requestRouter.post("/request-a-quote", requestController);
 
 module.exports = requestRouter;
